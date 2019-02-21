7b26effbc9304de1cf002c046a31c7c3d4ad41a2
Alyona Hulak <alena.hulak@gmail.com>
2019-02-21 17:00:40 +0200

    Update AppDelegate.swift
    hide API key

--- lun/AppDelegate.swift
+++ lun/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyDGN-oezMFgN7TqfUCNMIYsWbiq8_CrfGs")
+        GMSServices.provideAPIKey("INSERT YOUR API KEY HERE")
         return true
     }
 
